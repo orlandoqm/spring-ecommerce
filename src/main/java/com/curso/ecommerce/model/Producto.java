3c2ef4e3fb715a2aefff63365eb991324fd43b1e
orlandoqm <orlandoquirogam@gmail.com>
2025-05-16 16:31:45 -0500

    Creando primeros modelos

---
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.ecommerce.model;
+
+/**
+ *
+ * @author ORLANDO
+ */
+public class Producto {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String imagen;
+    private Double precion;
+    private int cantidad;
+
+    public Producto() {
+    }
+
+    public Producto(Integer id, String nombre, String descripcion, String imagen, Double precion, int cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+        this.precion = precion;
+        this.cantidad = cantidad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public Double getPrecion() {
+        return precion;
+    }
+
+    public void setPrecion(Double precion) {
+        this.precion = precion;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen + ", precion=" + precion + ", cantidad=" + cantidad + '}';
+    }
+    
+    
+}
---
+++ src/main/java/com/curso/ecommerce/model/Usuario.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.curso.ecommerce.model;
+
+/**
+ *
+ * @author ORLANDO
+ */
+public class Usuario {
+    private Integer id;
+    private String nombre;
+    private String userName;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private String tipo;
+    private String password;
+
+    public Usuario() {
+    }
+
+    
+    public Usuario(Integer id, String nombre, String userName, String email, String direccion, String telefono, String tipo, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.userName = userName;
+        this.email = email;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.tipo = tipo;
+        this.password = password;
+    }
+
+    
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombre=" + nombre + ", userName=" + userName + ", email=" + email + ", direccion=" + direccion + ", telefono=" + telefono + ", tipo=" + tipo + ", password=" + password + '}';
+    }
+    
+    
+    
+    
+    
+}
